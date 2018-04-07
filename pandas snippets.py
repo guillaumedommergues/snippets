400347ec89cfa59f4f9e02a9757a03c34a9725de
guillaumedommergues <guillaume.dommergues@gmail.com>
2018-04-07 11:33:50 -1000

    Update pandas snippets.py

--- pandas snippets.py
+++ pandas snippets.py
@@ -1,88 +1,83 @@
-# Notes on how to use the pandas library
-# The online documentation is wonderful. There are plenty of great blogs and tutorials to learn
-# This is meant as a cheatcheet for my colleagues as these are the most common operations in our current positions
 
+# purpose: illustrate some common pandas operations to manipulate tabular data
+# part of a series of lessons taught at Bank of Hawaii, 2017-2018
+
+from __future__ import division
 import pandas as pd
 import numpy as np
+from datetime import datetime
+#%%
 
-# read an excel/csv file
-data=pd.read_excel("data.xlsx", sheetname="Sheet1") #sheet_name on more recent pandas versions
-data=pd.read_csv("data/data.csv", 
-                 skiprows=1, #rows to skip
-                 parse_dates=[1], #columns to parse as datetime objects
-                 names=["Fare","Date","Distance","Duration"] #new columns names
-                 )
-# get general info about the dataframe
-data.info()
-data.describe()
+# read a file
+df=pd.read_csv('international-airline-passengers.csv',skipfooter=3)
+
+# print the first and last lines
+df.head()
+df.tail()
+
+# get general info about the dataframe  
+df.info()  
+df.describe() 
+
+# print the column names
+df.columns
+
+# find the count of rows/columns
+nrows=df.shape[0]
+ncols=df.shape[1]
+
+# rename the columns
+df=df.rename(columns={'Month':'time',
+    'International airline passengers: monthly totals in thousands. Jan 49 ? Dec 60':'passengers'})
 
 # convert the type of a column
-data['Fare'] = data['Fare'].astype(int)
+df['passengers']=df['passengers'].astype(float)
 
-# edit a cell
-data.at[1,'Fare']=12 # where 1 is the index
+# edit one value
+df.at[0,'passengers']=200
 
-# initialize a new column with 0 values
-data['new_column']=0
+# calculate some summary data
+psg_min=df['passengers'].min()
+psg_max=df['passengers'].max()
+psg_mean=df['passengers'].mean()
+psg_std_dev=df['passengers'].std()
 
-# creating a column with the quantiles (here deciles) of another column
-data['fare_deciles']=pd.qcut(data['Fare'], 10, labels=False)
+# create a column with simple calculations 
+df['psg_normalized']=(df['passengers']-psg_mean)/psg_std_dev
 
-# you can then export the quantils if you wish
-quantiles=data.groupby(by=['fare_deciles']).agg({'Fare':np.max})
-quantiles.to_excel('quantiles.xlsx')
+# create a column column based on if/else calculation 
+df['500+']=False
+df_500=df.loc[df['passengers']>=500,'500+']=True
 
+# create new columns by applying a function 
+df['psg_min_max_scaled']=df['passengers'].apply(lambda x:\
+  (x-psg_min)/(psg_max-psg_min))
 
-# Assign variables to that new column based on simple conditions
-data.loc[data['Fare']==0,'new_column']=1
+#create a column with deciles
+df['deciles']=pd.qcut(df['passengers'], 10, labels=False)
 
-# Assign variables to that new column based on complex conditions
-conditions=[(data['Fare']==12 & Data['Distance]>100),
-            (data['Fare']==1 & Data['Distance]<=10)]
-choices=[4,7]
-data['new_column'] = np.select(conditions, choices, default=1)
+# create a new dataframe based on cell values/column names
+df_500=df.loc[df['500+'],:]  #or df_500_scaled=df.loc[df['passengers']>=500,:]
+df_scaled=df.loc[:,['psg_normalized','psg_min_max_scaled']]
 
-# normalizing variables
-features_to_normalize = ['Distance', 'Duration', 'Fare']
-# Store scalings in a dictionary so we can convert back later
-scaled_features = {}
-for each in features_to_normalize:
-    mean, std = data[each].mean(), data[each].std()
-    scaled_features[each] = [mean, std]
-    data.loc[:, each] = (data[each] - mean)/std
+# create a new dataframe based on row/column index
+df_first_10=df.iloc[range(0,10),[2,3]]
 
-# aggregate variables
-nsf=nsf.groupby(by=["Distance","Duration"]).agg({"Fare":"sum"})
-nsf=nsf.reset_index()
+# randomly keep 90% of the data for train, 10% for test
+np.random.seed(0) #for reproductability
+sample = np.random.choice(df.index, size=int(len(df)*0.9), replace=False)  
+df_train, df_test = df.iloc[sample], df.drop(sample) 
 
-# one hot encoding
-data['Weekday']=data['Date'].apply(lambda x: x.weekday())
-data=pd.concat([data, pd.get_dummies(data['Weekday'], prefix='d')], axis=1)
-data=data.drop(['Weekday'], axis=1)
-                                    
-# Split off random 10% of the data for testing
-np.random.seed(21)
-sample = np.random.choice(data.index, size=int(len(data)*0.9), replace=False)
-data, test_data = data.ix[sample], data.drop(sample)
+# remove a column
+df=df.drop('psg_min_max_scaled', axis=1) 
 
-# Split into features and targets
-features, targets = data.drop('Target', axis=1), data['Target']
-features_test, targets_test = test_data.drop('Target', axis=1), test_data['Target']
+# aggregate data based on a criteria
+df['month']=df['time'].apply(lambda x:datetime.strptime(x,'%Y-%m'))
+df['month']=df['month'].apply(lambda x:x.month)
+avg_by_month=df.groupby(['month']).agg({'passengers':'mean'})
 
-# export data to excel
-data.to_excel("data.xlsx") 
+# change the index
+df=df.set_index('time')
 
-# export several dataframes df1,df2,df3 to several worksheets
-from pandas import ExcelWriter
-def save_xls(list_dfs,names, xls_path):
-    writer = ExcelWriter(xls_path)
-    for n, df in enumerate(list_dfs):
-        df.to_excel(writer,names[n])
-    writer.save()
-list_dfs=[df1,df2,df3]
-names=['df1','df2','df3']
-xls_path="data.xlsx"
-save_xls(list_dfs, xls_path) 
-
-
-
+# export the data
+avg_by_month.to_csv('averages.csv')
