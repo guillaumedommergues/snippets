7247d5e155507e0adca11703958a2cfc3264104f
guillaumedommergues <guillaume.dommergues@gmail.com>
2018-04-15 10:50:51 -1000

    Update google_cloud_snippets.py

--- google_cloud_snippets.py
+++ google_cloud_snippets.py
@@ -1,10 +1,10 @@
+""" Purpose: illustrate some common routines for web apps
+running on Google Cloud
+Part of a series of lessons taught at Bank of Hawaii, 2017-2018
 
-# purpose: illustrate some common routines for web apps running on Google Cloud
-# part of a series of lessons taught at Bank of Hawaii, 2017-2018
+"""
 
-###############################################################################
-###################### app.yaml file ##########################################
-###############################################################################
+# app.yaml file ###############################################################
 
 # This file specifies how the app will run
 # https://cloud.google.com/appengine/docs/standard/python/config/appref
@@ -39,10 +39,7 @@ automatic_scaling:
   max_pending_latency: 100ms
   max_concurrent_requests: 50
 
-
-###############################################################################
-###################### appengine_config.py file ###############################
-###############################################################################
+# appengine_config.py file ####################################################
 
 # pure python libraries do not need to be imported in the app.yaml file
 # install them in a /lib folder 
@@ -57,31 +54,28 @@ if on_appengine and os.name == 'nt':
   sys.platform = "Not Windows"
 vendor.add('lib')
 
-###############################################################################
-###################### using Google Cloud Storage #############################
-###############################################################################
+# using Google Cloud Storage ##################################################
 
 # Google Cloud Storage is the place to store files, pictures, videos, etc
 # uploading files
 from google.cloud import storage
 def upload_file_to_Google_storage(file,filename,size):
-  storage_client = storage.Client('my-project-name') #replace 'my-project-name' with your project name
-  bucket = storage_client.get_bucket('my-bucket-name.appspot.com') #replace 'my-bucket-name.appspot.com' with your bucket name
+  storage_client = storage.Client('my-project-name') 
+  bucket = storage_client.get_bucket('my-bucket-name.appspot.com') 
   blob = bucket.blob(filename)
-  blob.upload_from_file(file, size=size, content_type ='image/jpg', rewind =True) #replace 'image/jpg' with applicable content type
+  blob.upload_from_file(
+          file, size=size, content_type ='image/jpg', rewind =True) 
   blob.make_public() #only to make it publicly accessible
-  return blob.public_url, blob.name #the url that can be used the access the file and the file name to delete it
+  return blob.public_url, blob.name 
 
 # deleting files
 def delete_file_to_Google_storage(blob_name):
-  storage_client = storage.Client('my-project-name') #replace 'my-project-name' with your project name
-  bucket = storage_client.get_bucket('my-bucket-name.appspot.com') #replace 'my-bucket-name.appspot.com' with your bucket name
+  storage_client = storage.Client('my-project-name') 
+  bucket = storage_client.get_bucket('my-bucket-name.appspot.com') 
   blob = bucket.blob(blob_name)
   blob.delete()
 
-###############################################################################
-###################### using the Google Datastore #############################
-###############################################################################
+# using the Google Datastore ##################################################
 
 # the Google Datastore is a tool to store non-relational data
 # it is free for limited usage 
@@ -103,9 +97,8 @@ class marker_kind(ndb.Model):
   markerValue = ndb.FloatProperty()
 
 # creating an entity 
-p = marker_kind(markerTitle='my title',
-                markerDescription='my description',
-                markerValue=1)
+p = marker_kind(
+    markerTitle='my title', markerDescription='my description', markerValue=1)
 p.put() 
 
 # updating a value
@@ -113,27 +106,24 @@ p.markerDescription='new description'
 p.put() 
 
 # getting the key of an entity
-k=p.key
+k = p.key
 
 # a key consists of a kind and an ID 
 # can be useful to send a unique identifier as string from client to server 
-my_id=p.key.id()
-my_kind=p.key.kind()
+my_id = p.key.id()
+my_kind = p.key.kind()
 
 # listing all entities in a kind
-marker_list=marker_kind.query()
+marker_list = marker_kind.query()
 
 # querying a kind with some criteria
 # here the first 10 entities with the markerTitle 'my title'
-my_markers=marker_kind.query(marker_kind.markerTitle =='my title').fetch(10)
+my_markers = marker_kind.query(marker_kind.markerTitle == 'my title').fetch(10)
 
 # deleting an entity - if you know the key k
 k.delete()
 
-
-###############################################################################
-###################### linking an app to Google Cloud SQL #####################
-###############################################################################
+# linking an app to Google Cloud SQL ##########################################
 
 # SQL tables can be used via Google Cloud SQL 
 # and connected to an app running on the Google App Engine
